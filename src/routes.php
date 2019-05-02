5688b0b13835ac429484889c19ad29432b9330ad
Vinicius da Silva Lourenco <vinicius.vsl@gmail.com>
2019-05-01 21:04:47 -0300

    Create a template layout for the project

--- src/routes.php
+++ src/routes.php
@@ -4,9 +4,13 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 
+$app->get('/example', function($request, $response){
+    return $this->view->render($response, 'example.twig');
+});
+
 $app->get('/', function($request, $response){
     return $this->view->render($response, 'index.twig');
-});
+})->setName('home');
 
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
