db836fdefd0c2f3a9c0d64073f57d99a99ae4e0c
Vinicius da Silva Lourenco <vinicius.vsl@gmail.com>
2019-08-08 21:27:49 -0300

    Recalculate allocations to sum 100

--- src/controllers/ChartController.php
+++ src/controllers/ChartController.php
@@ -5,7 +5,7 @@ namespace App\Controllers;
 class ChartController extends Controller
 {    
     public function allocationType($request, $response){        
-        $products = $this->c->db->query("Select SUM(value) as value, product_type.name From product 
+        $products = $this->c->db->query("Select SUM(value) as value, product_type.name, product_type.color From product 
             Inner Join product_type on product_type.id = product.product_type_id
             WHERE product.deleted_at IS NULL
             Group By product_type.id")->fetchAll(\PDO::FETCH_OBJ);
@@ -13,8 +13,14 @@ class ChartController extends Controller
         foreach($products as $product){
             $chart_label[] = $product->{"name"};
             $chart_serie[] = $product->{"value"};
+            $chart_color[] = $product->{"color"};
+        }
+
+        $total = array_sum($chart_serie);
+        foreach ($chart_serie as $key => $value) {
+            $chart_serie[$key] = round($chart_serie[$key]/$total, 2)*100;
         }
         
-        return $this->c->view->render($response, 'chart_allocation_type.twig', compact('chart_label', 'chart_serie'));        
+        return $this->c->view->render($response, 'chart_allocation_type.twig', compact('chart_label', 'chart_serie', 'chart_color'));        
     }
 }
